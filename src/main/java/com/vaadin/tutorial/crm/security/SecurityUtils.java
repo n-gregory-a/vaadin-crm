7f5ca678427d8810a0f1a69c4dd5bbe6c1390cf5
Grigory <ovonely@gmail.com>
2020-04-17 20:54:01 +0700

    creating SecurityUtils class for utility methods

---
+++ src/main/java/com/vaadin/tutorial/crm/security/SecurityUtils.java
@@ -0,0 +1,32 @@
+package com.vaadin.tutorial.crm.security;
+
+import com.vaadin.flow.server.ServletHelper;
+import com.vaadin.flow.shared.ApplicationConstants;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Stream;
+
+public final class SecurityUtils {
+
+    public SecurityUtils() {
+
+    }
+
+    //determines if a request is internal to Vaadin
+    static boolean isFrameworkInternalRequest (HttpServletRequest request) {
+        final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
+        return parameterValue != null && Stream.of(ServletHelper.RequestType.values())
+                .anyMatch(r -> r.getIdentifier().equals(parameterValue));
+    }
+
+    //checks if the current user is logged in
+    static boolean isUserLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null
+                && !(authentication instanceof AnonymousAuthenticationToken)
+                && authentication.isAuthenticated();
+    }
+}
