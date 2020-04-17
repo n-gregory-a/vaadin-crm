f43b6ff02d904b4025e599843a74f2e2c142a808
Grigory <ovonely@gmail.com>
2020-04-17 21:17:52 +0700

    creating SecurityConfiguration class

---
+++ src/main/java/com/vaadin/tutorial/crm/security/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.vaadin.tutorial.crm.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String LOGIN_PROCESSING_URL = "/login";
+    private static final String LOGIN_FAILURE_URL = "/login?error";
+    private static final String LOGIN_URL = "/login";
+    private static final String LOGIN_SUCCESS_URL = "/login";
+
+}
