4dfe3fe689c4f600dfe27a08e12e2557ae2c3c35
Grigory <ovonely@gmail.com>
2020-04-18 21:30:47 +0700

    adding a method to block unauthenticated requests to all pages, except the login page

--- src/main/java/com/vaadin/tutorial/crm/security/SecurityConfiguration.java
+++ src/main/java/com/vaadin/tutorial/crm/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.vaadin.tutorial.crm.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,4 +14,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String LOGIN_URL = "/login";
     private static final String LOGIN_SUCCESS_URL = "/login";
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .requestCache().requestCache(new CustomRequestCache())
+                .and().authorizeRequests()
+                .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
+
+                .anyRequest().authenticated()
+
+                .and().formLogin()
+                .loginPage(LOGIN_URL).permitAll()
+                .loginProcessingUrl(LOGIN_PROCESSING_URL)
+                .failureUrl(LOGIN_FAILURE_URL)
+                .and().logout().logoutSuccessUrl(LOGIN_SUCCESS_URL);
+    }
 }
